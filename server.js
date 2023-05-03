f4b7262dfb95bdf4e488c250034be7b811161051
Tejass07 <tejassdesh07@gmail.com>
2023-05-04 01:17:51 +0530

    cDYGC

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require("./config/database");
 // const Member = require("./models/member");
 const { json } = require("express");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(static_path));
